8070965705a7612ce22658f77fac0411e476ac2c
Hussein debel <debelhussein@gmail.com>
2019-06-01 22:35:25 +1000

    Updates

---
+++ precache-manifest.0379caca6551acf133832f3f95c9f953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b9bbe418386a4354da",
+    "url": "/bballfrontend/static/js/runtime~main.ddb9dd5c.js"
+  },
+  {
+    "revision": "a8291e17947b56775811",
+    "url": "/bballfrontend/static/js/main.0c317080.chunk.js"
+  },
+  {
+    "revision": "502f5584561bbb736ff9",
+    "url": "/bballfrontend/static/js/2.12d04e59.chunk.js"
+  },
+  {
+    "revision": "a8291e17947b56775811",
+    "url": "/bballfrontend/static/css/main.c9c2caa7.chunk.css"
+  },
+  {
+    "revision": "56ae5d19d3492ffb39ebc7a08cc3e323",
+    "url": "/bballfrontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1fdfb277fa44fda09a36a49c4cf7999.js"
+  "/bballfrontend/precache-manifest.0379caca6551acf133832f3f95c9f953.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bballfrontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
