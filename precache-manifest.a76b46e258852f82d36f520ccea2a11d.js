4d64940aff78aaa798da299bdfe5e648eb37b9bd
Hussein debel <debelhussein@gmail.com>
2019-06-01 23:46:46 +1000

    Updates

--- precache-manifest.a76b46e258852f82d36f520ccea2a11d.js
+++ precache-manifest.a76b46e258852f82d36f520ccea2a11d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bballfrontend/static/js/runtime~main.ddb9dd5c.js"
   },
   {
-    "revision": "d492881d064e8f865056",
-    "url": "/bballfrontend/static/js/main.9f6e72cd.chunk.js"
+    "revision": "b5e8316c8ecee5b43115",
+    "url": "/bballfrontend/static/js/main.811fb2d7.chunk.js"
   },
   {
     "revision": "502f5584561bbb736ff9",
     "url": "/bballfrontend/static/js/2.12d04e59.chunk.js"
   },
   {
-    "revision": "d492881d064e8f865056",
+    "revision": "b5e8316c8ecee5b43115",
     "url": "/bballfrontend/static/css/main.c9c2caa7.chunk.css"
   },
   {
-    "revision": "099e0b4fa31f576a98ed25937b76d22f",
+    "revision": "3ad937574bdd7ebc930b8cfc9e4785ff",
     "url": "/bballfrontend/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bballfrontend/precache-manifest.763c04aab72277bd66b785808df511d1.js"
+  "/bballfrontend/precache-manifest.a76b46e258852f82d36f520ccea2a11d.js"
 );
 
 workbox.clientsClaim();
